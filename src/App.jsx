f0347099f59126a186d03fcb15e52370a5321041
SandraCarretero <sandracarretero24@gmail.com>
2024-06-05 14:15:05 +0200

    slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { GlobalStyles } from "./styles/GlobalStyles";
+import User from './components/user/User';
+import { GlobalStyles } from './styles/GlobalStyles';
 
 const App = () => {
 	return (
 		<>
-			<GlobalStyles/>
-			<h1>Núcleo de la aplicación</h1>;
+			<GlobalStyles />
+			<User />
 		</>
 	);
 };
---
+++ src/components/user/User.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import { USERS } from '../../constants/users';
+
+const User = () => {
+	const [currentIndex, setCurrentIndex] = useState(0);
+	const user = USERS[currentIndex];
+
+	const isDisabledNext = currentIndex === USERS.length - 1;
+	const isDisabledPrevious = currentIndex === 0;
+
+	return (
+		<div>
+			<div key={user.userId}>
+				<img src={user.profileImage} alt={`${user.name}'s profile`} />
+				<h1>Name: {user.name}</h1>
+				<p>Username: {user.username}</p>
+				<p>Email: {user.email}</p>
+				<button
+					disabled={isDisabledPrevious}
+					onClick={() => handlePrevious(currentIndex, setCurrentIndex)}
+				>
+					Previous
+				</button>
+				<button
+					disabled={isDisabledNext}
+					onClick={() => handleNext(currentIndex, setCurrentIndex)}
+				>
+					Next
+				</button>
+			</div>
+		</div>
+	);
+};
+
+const handleNext = (currentIndex, setCurrentIndex) => {
+	setCurrentIndex(currentIndex + 1);
+};
+
+const handlePrevious = (currentIndex, setCurrentIndex) => {
+	setCurrentIndex(currentIndex - 1);
+};
+
+export default User;
